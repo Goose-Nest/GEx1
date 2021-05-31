141282ced27a602389be48b433aa714f02679a26
Oj <oj@oojmed.com>
2021-05-31 09:51:43 +0100

    [Index] Add component system

--- src/index.js
+++ src/index.js
@@ -6,3 +6,11 @@ const readTheme = (path) => yaml.load(readFileSync(path));
 const theme = readTheme(process.argv[2]);
 
 console.log(theme);
+
+let css;
+
+for (const component in theme) {
+  css += (await import(`./components/${component}.js`)).default(theme);
+}
+
+console.log(css);
